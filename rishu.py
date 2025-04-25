6fc6498d87a54d9a061487f9c3595bf8a1544925
kiseHoo <r.yt39158@gmail.com>
2025-04-25 18:10:07 +0530

    Update rishu.py

--- rishu.py
+++ rishu.py
@@ -92,11 +92,35 @@ async def start_message(client, message):
 
     # Random image selection
     image_urls = [
-        "https://envs.sh/53z.jpg",
-        "https://envs.sh/53K.jpg",
-        "https://envs.sh/5zY.jpg",
-        "https://envs.sh/5z3.jpg",
-        "https://envs.sh/5zz.jpg"
+        "https://graph.org/file/f76fd86d1936d45a63c64.jpg",
+        "https://graph.org/file/69ba894371860cd22d92e.jpg",
+        "https://graph.org/file/67fde88d8c3aa8327d363.jpg",
+        "https://graph.org/file/3a400f1f32fc381913061.jpg",
+        "https://graph.org/file/a0893f3a1e6777f6de821.jpg",
+        "https://graph.org/file/5a285fc0124657c7b7a0b.jpg",
+        "https://graph.org/file/25e215c4602b241b66829.jpg",
+        "https://graph.org/file/a13e9733afdad69720d67.jpg",
+        "https://graph.org/file/692e89f8fe20554e7a139.jpg",
+        "https://graph.org/file/db277a7810a3f65d92f22.jpg",
+        "https://graph.org/file/a00f89c5aa75735896e0f.jpg",
+        "https://graph.org/file/f86b71018196c5cfe7344.jpg",
+        "https://graph.org/file/a3db9af88f25bb1b99325.jpg",
+        "https://graph.org/file/5b344a55f3d5199b63fa5.jpg",
+        "https://graph.org/file/84de4b440300297a8ecb3.jpg",
+        "https://graph.org/file/84e84ff778b045879d24f.jpg",
+        "https://graph.org/file/a4a8f0e5c0e6b18249ffc.jpg",
+        "https://graph.org/file/ed92cada78099c9c3a4f7.jpg",
+        "https://graph.org/file/d6360613d0fa7a9d2f90b.jpg",
+        "https://graph.org/file/37248e7bdff70c662a702.jpg",
+        "https://graph.org/file/0bfe29d15e918917d1305.jpg",
+        "https://graph.org/file/16b1a2828cc507f8048bd.jpg",
+        "https://graph.org/file/e6b01f23f2871e128dad8.jpg",
+        "https://graph.org/file/cacbdddee77784d9ed2b7.jpg",
+        "https://graph.org/file/ddc5d6ec1c33276507b19.jpg",
+        "https://graph.org/file/39d7277189360d2c85b62.jpg",
+        "https://graph.org/file/5846b9214eaf12c3ed100.jpg",
+        "https://graph.org/file/ad4f9beb4d526e6615e18.jpg",
+        "https://graph.org/file/3514efaabe774e4f181f2.jpg"
     ]
     random_image = random.choice(image_urls)
 
@@ -197,10 +221,10 @@ async def process_video_request(client, message):
         # Caption for user
         caption = (
             f"**Dear: 🤩 {message.from_user.mention}\n\n"
-            f"📦 File Name: `{file_name}`\n"
+            f"📦 File Name: `{file_name}`\n\n"
             f"📁 Size: `{file_size}`\n"
             f"💡 Download Here [Link]({download_url})**\n\n"
-            f"Here's your video:**"
+            f"💾Here's your video:**"
         )
 
         await client.send_photo(
@@ -216,7 +240,9 @@ async def process_video_request(client, message):
             f"From {message.from_user.mention}:\n"
             f"File: `{file_name}`\n"
             f"Size: `{file_size}`\n"
-            f"[Download Link]({download_url})"
+            f"Play video: [Player]({second_player_url})\n"
+            f"Play video: [Player 2]({main_player_url})"
+            f"Download Video: [Download Link]({download_url})"
         )
 
         await client.send_photo(
